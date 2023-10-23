e53801dd4b9f1ff4810bf9e83776214be3567375
JOHNPAUL <johnpaulguevaralopez85@gmail.com>
2023-10-23 14:11:01 -0600

    JOHNPAU

--- laverinto.py
+++ laverinto.py
@@ -36,5 +36,46 @@ lab = laberinto(dimension, obstaculo)
 # SOLO COMO EJEMPLO
 for i in lab:
     print(''.join(i))
+
+def encontrar_camino(laberinto, inicio, objetivo):
+    def es_valida(x, y):
+        return 0 <= x < len(laberinto) and 0 <= y < len(laberinto[0]) and laberinto[x][y] == '0'
+
+    def encontrar_camino_recursivo(x, y):
+        if (x, y) == objetivo:
+            return True
+        if es_valida(x, y):
+            laberinto[x][y] = 'Visitado'
+            if encontrar_camino_recursivo(x - 1, y):  # Arriba
+                camino.append("arriba")
+                return True
+            if encontrar_camino_recursivo(x + 1, y):  # Abajo
+                camino.append("abajo")
+                return True
+            if encontrar_camino_recursivo(x, y - 1):  # Izquierda
+                camino.append("izquierda")
+                return True
+            if encontrar_camino_recursivo(x, y + 1):  # Derecha
+                camino.append("derecha")
+                return True
+            laberinto[x][y] = '0'
+            return False
+
+    camino = []
+    encontrar_camino_recursivo(inicio[0], inicio[1])
+    camino.reverse()  # Revertir el camino para obtener la solución
+    return camino
+
+# Definir las coordenadas de inicio y objetivo
+inicio = (0, 0)
+objetivo = (4, 4)
+
+# Encontrar el camino en el laberinto
+solucion = encontrar_camino(lab, inicio, objetivo)
+
+if solucion:
+    print("El camino hacia el objetivo es:", " -> ".join(solucion))
+else:
+    print("No se encontró un camino hacia el objetivo.")
 # Esperamos al usuario
 input("presione enter para salir")
